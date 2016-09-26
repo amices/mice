c3b40f31421fc5f221786caacdc0c3fa353751bf
RianneSchouten <r.m.schouten@uu.nl>
2016-09-26 14:19:04 +0200

    First start on new approach!
    Use lists to insert patterns into the function

---
+++ R/AmputeNew.R
@@ -0,0 +1,14 @@
+
+#'@export
+ampute <- function(data, patterns, prop = 0.5, bycases = TRUE, run = TRUE) {
+  
+  
+  
+}
+                   
+pattern <- list(pat = c(0, 1, 1, 1),
+                mech = "MAR", 
+                weights = c(0, 1, 1, 1), 
+                cont = "TRUE", 
+                type = "MARRIGHT", 
+                size = 0.5)
