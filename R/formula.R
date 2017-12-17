46607f19ff5d8d6070685defcda17b5d7c3acd09
Stef van Buuren <stef.vanbuuren@tno.nl>
2017-12-17 23:10:10 +0100

    Add name.formulas

--- R/formula.R
+++ R/formula.R
@@ -3,17 +3,16 @@
 #' @param formula A formula. If it is 
 #' not a formula, the formula is internally reset to \code{~0}.
 #' @param predictors A character vector of variable names.
-#' @param include.auxiliary A logical that indicates whether the variables
+#' @param auxiliary A logical that indicates whether the variables
 #' listed in \code{predictors} should be added to the formula as main 
-#' effects. The default is \code{TRUE}.
+#' effects. The default is \code{FALSE}.
 #' @param include.intercept A logical that indicated whether the intercept 
-#' should be included in the result. The default (\code{FALSE}) may change 
-#' in future versions.
+#' should be included in the result.
 #' @return A formula
 #' @keywords internal
 extend.formula <- function(formula = ~ 0,
                            predictors = NULL,
-                           include.auxiliary = TRUE,
+                           auxiliary = FALSE,
                            include.intercept = FALSE, ...) {
   if (!is.formula(formula)) formula <- ~ 0
   
@@ -26,7 +25,7 @@ extend.formula <- function(formula = ~ 0,
   } else 
     fr <- reformulate(c(".", predictors))
   
-  if (include.auxiliary) formula <- update(formula, fr, ...)
+  if (auxiliary) formula <- update(formula, fr, ...)
   if (include.intercept) formula <- update(formula, ~ . + 1, ...)
   formula
 }
@@ -40,4 +39,77 @@ hasdot <- function(f) {
     return(any(sapply(as.list(f), hasdot)))
   } else {
     f == as.symbol(".")}
+}
+
+#' Name formula list elements
+#'
+#' This helper function names any unnaned elements in the \code{formula} 
+#' list. This is a convenience function.
+#' @inheritParams mice
+#' @param prefix A character vector of length 1 with the prefix to
+#' be using for naming any unnamed blocks with two or more variables.
+#' @return Named list of formulas
+#' @seealso \code{\link{mice}}
+#' @details 
+#' This function will name any unnamed list elements specified in 
+#' the optional argument \code{formula}. Unnanmed formula's 
+#' consisting with just one response variable will be named 
+#' after this variable. Unnamed formula's containing more 
+#' than one variable will be named by the \code{prefix} 
+#' argument, padded by an integer sequence stating at 1.
+#' @examples
+#' # fully conditionally specified main effects model
+#' form1 <- list(bmi ~ age + chl + hyp, 
+#'               hyp ~ age + bmi + chl,
+#'               chl ~ age + bmi + hyp)
+#' form1 <- name.formulas(form1)
+#' imp1 <- mice(nhanes, formulas = form1, print = FALSE, m = 1, seed = 12199)
+#' 
+#' # same model using dot notation
+#' form2 <- list(bmi ~ ., hyp ~ ., chl ~ .)
+#' form2 <- name.formulas(form2, data = nhanes)
+#' imp2 <- mice(nhanes, formulas = form2, print = FALSE, m = 1, seed = 12199)
+#' identical(complete(imp1), complete(imp2))
+#' 
+#' # same model using predictorMatrix
+#' imp3 <- mice(nhanes, print = FALSE, m = 1, seed = 12199, auxiliary = TRUE)
+#' identical(complete(imp1), complete(imp3))
+#' 
+#' # multivariate imputation for chl and bmi
+#' form4 <- list(chl + bmi ~ ., hyp ~ bmi + age)
+#' form4 <- name.formulas(form3, data = nhanes)
+#' imp4 <- mice(nhanes, formulas = form3, print = FALSE, m = 1, seed = 71712)
+#' @export
+name.formulas <- function(formulas, data = NULL, prefix = "F") {
+  if (!is.list(formulas))
+    stop("Argument `formulas` not a list", call. = FALSE)
+  if (!all(sapply(formulas, is.formula)))
+    stop("Not all elements in `formulas` are a formula")
+  if (is.null(names(formulas))) names(formulas) <- rep("", length(formulas))
+  inc <- 1
+  for (i in seq_along(formulas)) {
+    if (names(formulas)[i] != "") next
+    if (hasdot(formulas[[i]]) && is.null(data)) 
+      stop("Formula with dot requires `data` argument", call. = FALSE)
+    y <- lhs(formulas[[i]], data = data)
+    if (length(y) == 1) names(formulas)[i] <- y
+    else {
+      names(formulas)[i] <- paste0(prefix, inc)
+      inc <- inc + 1
+    }
+  }
+  formulas
+}
+
+lhs <- function(formula, data) {
+  # taken from mitml
+  ft <- terms(formula, data = data)
+  tl <- attr(ft,"term.labels")
+  vrs <- attr(ft,"variables")[-1]
+
+  yvrs <- as.character(vrs)[attr(ft,"response")]
+  yvrs <- gsub("[\r\n]","",yvrs)
+  y.fml <- as.formula(paste0("~",yvrs))
+  yvrs <- attr(terms(y.fml), "term.labels")
+  yvrs
 }
--- tests/testthat/test-check.formula.R
+++ tests/testthat/test-check.formula.R
@@ -1,4 +1,4 @@
-context("check.formula")
+context("check.formulas")
 
 data <- nhanes
 where <- is.na(data)
@@ -9,29 +9,49 @@ ini <- mice(data, blocks = blocks, maxit = 0)
 # classic type specification
 setup <- list(blocks = blocks, 
               predictorMatrix = ini$predictorMatrix,
-              formula = NULL)
-v1 <- mice:::check.formula(setup, data)
+              formulas = NULL)
+v1 <- mice:::check.formulas(setup, data)
 
 # using a formula
-formula <- v1$formula
+formulas <- v1$formulas
 setup <- list(blocks = blocks, 
               predictorMatrix = ini$predictorMatrix,
-              formula = formula)
-v2 <- mice:::check.formula(setup, data)
-v2$formula
+              formulas = formulas)
+v2 <- mice:::check.formulas(setup, data)
+v2$formulas
 
 test_that("updates `has.formula` attribute", {
-  expect_false(identical(v2$formula, v2$formula.arg))
-  expect_identical(v2$formula[[1]], v2$formula.arg[[1]])
+  expect_false(identical(v2$formulas, v2$formulas.arg))
+  expect_identical(v2$formulas[[1]], v2$formulas.arg[[1]])
 })
 
 # try dot in formula
-formula <- list(bmi ~ ., age ~ ., chl ~ .)
+formulas <- list(bmi ~ ., age ~ ., chl ~ .)
+formulas <- name.formulas(formulas)
 setup <- list(blocks = blocks, 
               predictorMatrix = ini$predictorMatrix,
-              formula = formula)
-v3 <- mice:::check.formula(setup, data)
-v3$formula
+              formulas = formulas)
+v3 <- mice:::check.formulas(setup, data)
+v3$formulas
+
+# classic specification using predictorMatrix
+imp1 <- mice(nhanes, seed = 51212, print = FALSE, m = 1)
+cmp1 <- complete(imp1)
+
+# formula specification
+form <- list(age ~ ., bmi ~ ., hyp ~., chl ~ .)
+imp2 <- mice(nhanes, formulas = form, seed = 51212, print = FALSE, m = 1)
+cmp2 <- complete(imp2)
+
+test_that("predictorMatrix and formula yield same imputations", {
+  expect_identical(cmp1, cmp2)
+  expect_identical(imp1$imp, imp2$imp)
+})
+
+# formula specification
+form <- name.blocks(list(bmi ~ ., hyp ~., chl ~ .))
+imp3 <- mice(nhanes, formulas = form, seed = 51212, print = FALSE, m = 1)
+cmp3 <- complete(imp3)
 
 
 # old.form <- c("", "bmi ~ chl + hyp", "hyp ~ bmi + chl", "chl ~ bmi + hyp")
@@ -43,10 +63,10 @@ v3$formula
 #              print = FALSE, seed = 1)
 # # impute the mean
 # imp2 <- mice(nhanes, formula = form1, m = 1, maxit = 2, method = "norm.predict",
-#              print = FALSE, include.auxiliary = FALSE, seed = 1)
+#              print = FALSE, seed = 1)
 # 
 # form2 <- list(bmi = "hyp ~ 1", chl = "hyp ~ 1", hyp = "hyp ~ 1")
 # imp3 <- mice(nhanes, formula = form2, m = 1, maxit = 2, method = "norm.predict",
-#              print = FALSE, include.auxiliary = FALSE, seed = 1)
+#              print = FALSE, seed = 1)
 # 
 
