1639edede21c55767c8dba5c198eb34c747c284a
Stef van Buuren <stef.vanbuuren@tno.nl>
2017-12-20 15:46:15 +0100

    Generalize and document extract.blocks()

--- R/blocks.R
+++ R/blocks.R
@@ -114,7 +114,49 @@ check.blocks <- function(blocks, data) {
   blocks
 }
 
-extract.blocks <- function(formulas) {
-  if (!all(sapply(formulas, is.formula))) return(NULL)
-  name.blocks(lapply(formulas, lhs))
+#' Extract blocks from \code{formulas} and \code{predictorMatrix} 
+#'
+#' This helper function attempts to find blocks of variables in the
+#' specification of the \code{formulas} and/or \code{predictorMatrix}
+#' objects. Blocks specified by \code{formulas} may consist of 
+#' multiple variables. Blocks specified by \code{predictorMatrix} are
+#' assumed to consist of single variables. Any duplicates in names are 
+#' removed, and the formula specification is preferred.
+#' \code{predictorMatrix} and \code{formulas}. When both arguments 
+#' specify models for the same block, the model for the 
+#' \code{predictMatrix} is removed, and priority is given to the 
+#' specification given in \code{formulas}. 
+#' @inheritParams mice 
+#' @return A \code{blocks} object.
+#' @seealso \code{\link{make.blocks}}, \code{\link{name.blocks}}
+#' @examples
+#' form <- name.formulas(list(bmi + hyp ~ chl + age, chl ~ bmi))
+#' pred <- make.predictorMatrix(nhanes[, c("age", "chl")])
+#' extract.blocks(formulas = form, pred = pred)
+#' @export
+extract.blocks <- function(formulas = NULL, predictorMatrix = NULL) {
+  
+  blocks.f <- blocks.p <- NULL
+  if (!is.null(formulas)) {
+    if (!all(sapply(formulas, is.formula))) return(NULL)
+    blocks.f <- name.blocks(lapply(name.formulas(formulas), lhs))
+    attr(blocks.f, "model") <- rep("form", length(blocks.f))
+    if (is.null(predictorMatrix)) return(blocks.f)
+  }
+  
+  if (!is.null(predictorMatrix)) {
+    if (is.null(row.names(predictorMatrix)))
+      stop("No row names in predictorMatrix", call. = FALSE)
+    blocks.p <- name.blocks(row.names(predictorMatrix))
+    attr(blocks.p, "model") <- rep("pred", length(blocks.p))
+    if (is.null(formulas)) return(blocks.p)
+  }
+  
+  blocknames <- unique(c(names(blocks.f), names(blocks.p)))
+  keep <- setdiff(blocknames, names(blocks.f))
+  blocks <- c(blocks.f, blocks.p[keep])
+  attr(blocks, "model") <- c(rep("form", length(formulas)), 
+                             rep("pred", length(keep)))
+  blocks
 }
+
