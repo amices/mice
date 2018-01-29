f0e09b742e2171c1d09f08d7f449b281b7b373ad
Stef van Buuren <stef.vanbuuren@tno.nl>
2018-01-29 12:30:54 +0100

    Add tests for miceadds and micemd

--- tests/testthat/test-mice.impute.2l.bin.R
+++ tests/testthat/test-mice.impute.2l.bin.R
@@ -1,42 +1,70 @@
 context("mice.impute.2l.bin")
-# library(mice) 
-# library(dplyr)
-# library(tidyr, warn.conflicts = FALSE)
-data(toenail, package = "DPpackage")
-data <- tidyr::complete(toenail, ID, visit) %>% 
-  tidyr::fill(treatment) %>% 
-  dplyr::select(-month)
 
+suppressPackageStartupMessages(library(miceadds, warn.conflicts = FALSE, quietly = TRUE))
+packageVersion(c("miceadds"))
+suppressPackageStartupMessages(library(micemd, warn.conflicts = FALSE, quietly = TRUE))
+packageVersion(c("micemd"))
+
+# toenail: outcome is factor
+data("toenail", package = "HSAUR3")
+data <- tidyr::complete(toenail, patientID, visit) %>% 
+  tidyr::fill(treatment) %>% 
+  dplyr::select(-time) %>%
+  dplyr::mutate(patientID = as.integer(patientID))
+summary(data)
 # fit1 <- glm(outcome ~ treatment * month, data = toenail, family = binomial)
 # fit2 <- glm(outcome ~ treatment * visit, data = toenail, family = binomial)
 # fit3 <- lme4::glmer(outcome ~ treatment * visit + (1 | ID), data = data, family = binomial)
 
-pred <- mice(data, print = FALSE, maxit = 0, seed = 1)$pred
-pred["outcome", "ID"] <- -2
-imp <- mice(data, method = "2l.bin", pred = pred, m = 1, maxit = 1, print = FALSE)
+pred <- make.predictorMatrix(data)
+pred["outcome", "patientID"] <- -2
 
-test_that("mice.impute.2l.bin() is silent", {
-  expect_silent(mice(data, method = "2l.bin", print = FALSE, pred = pred, m = 1, maxit = 1))
+test_that("mice::mice.impute.2l.bin() accepts factor outcome", {
+  expect_silent(imp <- mice(data, method = "2l.bin", print = FALSE, pred = pred, m = 1, maxit = 1))
+  expect_false(anyNA(complete(imp)))
 })
 
+# test_that("miceadds::mice.impute.2l.binary() accepts factor outcome", {
+#   expect_silent(imp <- mice(data, method = "2l.binary", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+#   expect_false(anyNA(complete(imp)))
+# })
+
+# test_that("micemd::mice.impute.2l.2stage.bin() accepts factor outcome", {
+#   expect_silent(imp <- mice(data, method = "2l.2stage.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+#   expect_false(anyNA(complete(imp)))
+#  })
+
+# test_that("micemd::mice.impute.2l.glm.bin() accepts factor outcome", {
+#   expect_silent(imp <- mice(data, method = "2l.glm.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+#   expect_false(anyNA(complete(imp)))
+# })
+
+
+# toenail: outcome is 0/1
 data("toenail", package = "DPpackage")
 data <- tidyr::complete(toenail, ID, visit) %>% 
   tidyr::fill(treatment) %>% 
   dplyr::select(-month)
 summary(data)
-
-suppressPackageStartupMessages(library(miceadds, warn.conflicts = FALSE, quietly = TRUE))
 pred <- make.predictorMatrix(data)
 pred["outcome", "ID"] <- -2
-test_that("miceadds::mice.impute.2l.binary() is silent", {
-  expect_silent(mice(data, method = "2l.binary", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
-})
-test_that("miceadds::mice.impute.2l.glm.bin() is silent", {
-  expect_silent(mice(data, method = "2l.glm.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+
+test_that("mice::mice.impute.2l.bin() accepts 0/1 outcome", {
+  expect_silent(imp <- mice(data, method = "2l.bin", print = FALSE, pred = pred, m = 1, maxit = 1))
+  expect_false(anyNA(complete(imp)))
 })
 
-suppressPackageStartupMessages(library(micemd, warn.conflicts = FALSE, quietly = TRUE))
-test_that("micemd::mice.impute.2l.2stage.bin() is silent", {
-  expect_silent(mice(data, method = "2l.2stage.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+test_that("miceadds::mice.impute.2l.binary() accepts 0/1 outcome", {
+  expect_silent(imp <- mice(data, method = "2l.binary", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+  expect_false(anyNA(complete(imp)))
 })
 
+# test_that("micemd::mice.impute.2l.2stage.bin() accepts 0/1 outcome", {
+#   expect_silent(imp <- mice(data, method = "2l.2stage.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+#   expect_false(anyNA(complete(imp)))
+# })
+
+test_that("micemd::mice.impute.2l.glm.bin() accepts 0/1 outcome", {
+  expect_silent(imp <- mice(data, method = "2l.glm.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+  expect_false(anyNA(complete(imp)))
+})
