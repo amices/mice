55e41e780fccb47eb1b65d2a8b782b30261e2478
Stef van Buuren <stef.vanbuuren@tno.nl>
2018-01-29 09:15:33 +0100

    Add tests for miceadds and micemd 2l.bin functions

--- tests/testthat/test-mice.impute.2l.bin.R
+++ tests/testthat/test-mice.impute.2l.bin.R
@@ -18,3 +18,25 @@ imp <- mice(data, method = "2l.bin", pred = pred, m = 1, maxit = 1, print = FALS
 test_that("mice.impute.2l.bin() is silent", {
   expect_silent(mice(data, method = "2l.bin", print = FALSE, pred = pred, m = 1, maxit = 1))
 })
+
+data("toenail", package = "DPpackage")
+data <- tidyr::complete(toenail, ID, visit) %>% 
+  tidyr::fill(treatment) %>% 
+  dplyr::select(-month)
+summary(data)
+
+suppressPackageStartupMessages(library(miceadds, warn.conflicts = FALSE, quietly = TRUE))
+pred <- make.predictorMatrix(data)
+pred["outcome", "ID"] <- -2
+test_that("miceadds::mice.impute.2l.binary() is silent", {
+  expect_silent(mice(data, method = "2l.binary", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+})
+test_that("miceadds::mice.impute.2l.glm.bin() is silent", {
+  expect_silent(mice(data, method = "2l.glm.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+})
+
+suppressPackageStartupMessages(library(micemd, warn.conflicts = FALSE, quietly = TRUE))
+test_that("micemd::mice.impute.2l.2stage.bin() is silent", {
+  expect_silent(mice(data, method = "2l.2stage.bin", pred = pred, seed = 1, maxit = 1, m = 1, print = FALSE))
+})
+
