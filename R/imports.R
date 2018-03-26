3ad161caa69ea19c5a98ad53e548790ca59a6337
Stef van Buuren <stef.vanbuuren@tno.nl>
2018-03-26 15:46:19 +0200

    Add lwd parameter to fluxplot()

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,7 @@
 #'@importFrom splines   bs
 #'@importFrom survival  basehaz coxph Surv
 #'@importFrom grDevices dev.capabilities hcl palette
-#'@importFrom graphics  abline points text plot
+#'@importFrom graphics  abline points text plot axis box
 #'@importFrom stats     C aggregate as.formula binomial coef
 #'                      contr.treatment cor df.residual fitted
 #'                      formula gaussian model.frame model.matrix
