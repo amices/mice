b02b11fe679372b315792e703681a81389559b69
Gerko Vink <g.vink@uu.nl>
2018-05-28 17:20:30 +0200

    allow for single pattern

--- R/md.pattern.R
+++ R/md.pattern.R
@@ -60,16 +60,21 @@ md.pattern <- function(x, plot = TRUE){
     mpat <- t(as.matrix(mpat, byrow = TRUE))
     rownames(mpat) <- table(pat)
   } else {
+    if(is.null(dim(mpat))){
+      mpat <- t(as.matrix(mpat))
+    }
     rownames(mpat) <- table(pat)
   }
   r <- cbind(abs(mpat - 1), rowSums(mpat))
   r <- rbind(r, c(nmis[order(nmis)], sum(nmis)))
-  class(r) <- "mdpat"
   if (plot){ #add plot
     plot.new()
-    if (all(!is.na(x))){
+    if (is.null(dim(sortR[!duplicated(sortR), ]))){
       R <- t(as.matrix(r[1:nrow(r)-1, 1:ncol(r)-1]))
     } else {
+      if(is.null(dim(R))){
+        R <- t(as.matrix(R))
+      }
       R <- r[1:nrow(r)-1, 1:ncol(r)-1]
     }
     par(mar=rep(0, 4))
@@ -90,40 +95,4 @@ md.pattern <- function(x, plot = TRUE){
   } else {
     return(r)
   }
-}
-
-#'Plot the missing data pattern
-#'
-#'This function is useful for visually inspecting the structure of missing
-#'observations in the data.
-#'
-#'@param x An object of class \code{mdpat}, produced by \code{md.pattern()}
-#'@return A rectangle plot of the missing data pattern, where the numerical pattern and 
-#'missingness information is contained in the margins.
-#'@author Gerko Vink, 2018
-#'@seealso \code{\link{md.pattern}}
-#'@method plot md.pattern
-#'@export
-plot.mdpat <- function(x){
-  plot.new()
-  nmis <- r[dim(x)[1], 1:dim(x)[2] - 1]
-  if (all(tail(x, n=1) == 0)){
-    R <- t(as.matrix(x[1:nrow(x)-1, 1:ncol(x)-1]))
-  } else {
-    R <- x[1:nrow(x)-1, 1:ncol(x)-1]
-  }
-  par(mar=rep(0, 4))
-  plot.window(xlim=c(-1, ncol(R) + 1), ylim=c(-1, nrow(R) + 1), asp=1)
-  M <- cbind(c(row(R)), c(col(R))) - 1
-  shade <- ifelse(R[nrow(R):1, ], mdc(1), mdc(2))
-  rect(M[, 2], M[, 1], M[, 2] + 1, M[, 1] + 1, col=shade)
-  for(i in 1:ncol(R)){
-    text(i - .5, nrow(R) + .3, colnames(x)[i], adj = c(0.5,0))
-    text(i - .5, -.3, nmis[i])
-  }
-  for(i in 1:nrow(R)){
-    text(ncol(R) + .3, i - .5, x[(nrow(x)-1):1, ncol(x)][i], adj = 0)
-    text(-.3, i - .5, rownames(x)[(nrow(x)-1):1][i], adj = 1)
-  }
-  text(ncol(R) + .3,  -.3, x[nrow(x), ncol(x)])
 }
