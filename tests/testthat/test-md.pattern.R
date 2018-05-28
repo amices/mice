5a84d887e3f91897cfb61df471d4dd2cd1dec948
Gerko Vink <g.vink@uu.nl>
2018-05-28 17:28:38 +0200

    elaborated comments

--- tests/testthat/test-md.pattern.R
+++ tests/testthat/test-md.pattern.R
@@ -5,13 +5,13 @@ test_that("patterns run as expected", {
   expect_silent(md.pattern(boys, plot = FALSE))
   #nhanes
   expect_silent(md.pattern(nhanes, plot = FALSE))
-  #one whole column missing (single pattern) - should not produce message
+  #one whole column missing (single pattern) - should not produce output
   expect_silent(md.pattern(cbind(na.omit(nhanes), NA), plot = FALSE))
-  #no missings (no pattern) - should produce message
+  #no missings (no pattern) - should produce output
   expect_output(md.pattern(na.omit(nhanes), plot = FALSE))
   #feed single column - expect error
   expect_error(md.pattern(nhanes$bmi))
-  #feed other than dataframe/matrix
+  #feed other than dataframe/matrix - expect error
   expect_error(md.pattern(as.list(nhanes)))
   #
 })
