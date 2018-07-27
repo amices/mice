f4307e2bf602e9aea5b932457d1d9d258f4f7fff
Stef van Buuren <stef.vanbuuren@tno.nl>
2018-07-27 09:16:45 +0200

    Solve symridge problem #119

--- tests/testthat/test-mice.impute.2l.norm.R
+++ tests/testthat/test-mice.impute.2l.norm.R
@@ -4,9 +4,7 @@ d1 <- brandsma[1:200, c("sch", "lpo")]
 pred <- make.predictorMatrix(d1)
 pred["lpo", "sch"] <- -2
 
-# OUTCOMMENTED #129
-# test_that("mice::mice.impute.2l.norm() runs empty model", {
-#   expect_silent(imp <- mice(d1, method = "2l.norm", print = FALSE, pred = pred, m = 1, maxit = 1))
-#   expect_false(anyNA(complete(imp)))
-# })
-
+test_that("mice::mice.impute.2l.norm() runs empty model", {
+  expect_silent(imp <- mice(d1, method = "2l.norm", print = FALSE, pred = pred, m = 1, maxit = 1))
+  expect_false(anyNA(complete(imp)))
+})
