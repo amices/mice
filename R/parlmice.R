ecc47ef7bafd3ab8ef07fe526022e1d3c26bd0d1
Paul Chang (ECON) <pachang@microsoft.com>
2018-08-03 10:49:12 -0700

    Fixex to parlmice.R to allow it to be called from other R functions.  This fix unpacks args and limits the environment passed to parallel::clusterExport.

--- R/parlmice.R
+++ R/parlmice.R
@@ -108,14 +108,20 @@ parlmice <- function(data, m = 5, seed = NA, cluster.seed = NA, n.core = NULL,
   # create arguments to export to cluster
   args <- match.call(mice, expand.dots = TRUE)
   args[[1]] <- NULL
+  args$data <-data
   args$m <- n.imp.core
+  
+  unpackedArgs = list(...)
+  for (key in names(unpackedArgs)){
+    args[key] = unpackedArgs[key]
+  }
 
   # make computing cluster
   cl <- parallel::makeCluster(n.core, type = cl.type)
   parallel::clusterExport(cl, 
                           varlist = c("data", "m", "seed", "cluster.seed", 
                                       "n.core", "n.imp.core", "cl.type",
-                                      ls(parent.frame())), 
+                                      ls(environment(parlmice))), 
                           envir = environment())
   parallel::clusterExport(cl, 
                           varlist = "do.call")
