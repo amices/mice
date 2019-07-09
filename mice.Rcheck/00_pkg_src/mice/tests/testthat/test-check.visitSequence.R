377edeb1b21ade3e2fee0f6223dd3709a52ae845
Stef van Buuren <stef.vanbuuren@tno.nl>
2019-07-09 09:46:27 +0200

    Remove misplaced mice.Rcheck folder

--- mice.Rcheck/00_pkg_src/mice/tests/testthat/test-check.visitSequence.R
+++
@@ -1,8 +0,0 @@
-context("check.visitSequence")
-
-data <- nhanes
-
-test_that("mice() takes numerical and character visitSequence", {
-  expect_silent(imp <- mice(data, visitSequence = 4:1, m = 1, print = FALSE))
-  expect_silent(imp <- mice(data, visitSequence = rev(names(data)), m = 1, print = FALSE))
-})
--- mice.Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(mice)
-
-test_check("mice")
