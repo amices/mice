cf0a2dc7d62a03d71d0521632be1be3e7fa82a18
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-02-11 22:45:06 +0100

    Add NARFCS imputation functions

---
+++ R/mice.impute.mnar.logreg.R
@@ -0,0 +1,47 @@
+#' @rdname mice.impute.mnar
+#' @export
+mice.impute.mnar.logreg <- function (y, ry, x, wy = NULL, 
+                                     ums = NULL, umx = NULL, ...) {
+  
+  ## Undentifiable part:
+  u <- parse.ums(x, ums = ums, umx = umx, ...)
+  
+  if (is.null(wy)) wy <- !ry
+  wyold <- wy
+  
+  ## Identifiable part: exactly the same as mice.impute.logreg
+  
+  # augment data in order to evade perfect prediction
+  aug <- augment(y, ry, x, wy)
+  x <- aug$x
+  y <- aug$y
+  ry <- aug$ry
+  wy <- aug$wy
+  w <- aug$w
+  
+  # fit model
+  x <- cbind(1, as.matrix(x))
+  expr <- expression(glm.fit(x = x[ry, , drop = FALSE], 
+                             y = y[ry], 
+                             family = quasibinomial(link = logit), 
+                             weights = w[ry]))
+  fit <- eval(expr)
+  fit.sum <- summary.glm(fit)
+  beta <- coef(fit)
+  rv <- t(chol(sym(fit.sum$cov.unscaled)))
+  beta.star <- beta + rv %*% rnorm(ncol(rv))
+  
+  ## Draw imputations
+  p <- 1/(1 + exp(-(x[wy, , drop = FALSE] %*% beta.star + 
+                      u$x[wyold, , drop = FALSE] %*% u$delta)))
+  vec <- (runif(nrow(p)) <= p)
+  vec[vec] <- 1
+  if (is.factor(y)) {
+    vec <- factor(vec, c(0, 1), levels(y))
+  }
+  
+  return(vec)
+}
+
+
+
---
+++ R/parse.ums.R
@@ -0,0 +1,26 @@
+parse.ums <- function(x, ums = NULL, umx = NULL, ...) {
+  if (is.null(ums)) stop("Unidentifiable model specification (ums) not found.")
+  if (!is.null(umx)) x <- base::cbind(x, umx)
+  
+  ## Unidentifiable part  
+  # e.g. specified in blots as list(X = list(ums = "-3+2*bmi"))
+  mnar0 <- gsub("-", "+-", ums)
+  mnar0 <- unlist(strsplit(mnar0, "+", fixed = TRUE))
+  if (mnar0[1L] == "") mnar0 <- mnar0[-1L]
+  if (sum(!grepl("*", mnar0, fixed = TRUE)) == 0L)
+    stop("An intercept (constant) term must be included in the expression")
+  else
+    if (sum(!grepl("*", mnar0, fixed = TRUE)) == 1L)
+      mnar0[!grepl("*", mnar0, fixed = TRUE)] <- paste(mnar0[!grepl("*", mnar0, fixed = TRUE)], "*intercept", sep = "")
+  else
+    if (sum(!grepl("*", mnar0, fixed = TRUE)) > 1L)
+      stop("Only one intercept term allowed")
+  mnar <- strsplit(mnar0, "*", fixed = TRUE)
+  mnar.parm <- as.numeric(unlist(lapply(mnar, function(x) x[1L]))) #e.g. c("-3","2")
+  mnar.vars <- unlist(lapply(mnar, function(x) x[2L])) #e.g. c("intercept","bmi")
+  mnar.parm <- mnar.parm[c(which(mnar.vars == "intercept"), which(mnar.vars != "intercept"))]
+  mnar.vars <- mnar.vars[c(which(mnar.vars == "intercept"), which(mnar.vars != "intercept"))]
+  xmnar <- as.matrix(cbind(1, as.matrix(x[ ,mnar.vars[!mnar.vars == "intercept"]])))
+  
+  list(delta = mnar.parm, x = xmnar)
+}
