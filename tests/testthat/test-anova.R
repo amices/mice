00adde40212467829577a367352233367f418baa
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-02-13 10:38:25 +0100

    Add testthat conditions

--- tests/testthat/test-anova.R
+++ tests/testthat/test-anova.R
@@ -5,8 +5,12 @@ m1 <- with(imp, lm(chl ~ bmi))
 m0 <- with(imp, lm(chl ~ 1))
 
 # anova methods
-z1 <- anova(m2, m1, m0)
-z2a <- anova(m2, m1, m0, method = "D2")
-z2b <- anova(m2, m1, m0, method = "D2", use = "likelihood")
-z3 <- anova(m2, m1, m0, method = "D3")
-
+test_that("anova.mira() produces silent D1 and D3", {
+  expect_silent(z1 <- anova(m2, m1, m0))
+  expect_silent(z3 <- anova(m2, m1, m0, method = "D3"))
+})
+  
+test_that("anova.mira() produces warnings on D2", {
+  expect_warning(z2a <- anova(m2, m1, m0, method = "D2"))
+  expect_warning(z2b <- anova(m2, m1, m0, method = "D2", use = "likelihood"))
+})
