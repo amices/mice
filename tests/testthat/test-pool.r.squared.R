7cca985dec8d15d5507d3a0c78f64eddaa63bfde
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2020-06-03 09:23:01 -0400

    test-pool.r.squared.R

---
+++ tests/testthat/test-pool.r.squared.R
@@ -0,0 +1,18 @@
+context("pool.r.squared")
+
+data(nhanes)
+imp <- mice::mice(nhanes, maxit = 2, m = 2, seed = 10, print = FALSE)
+fit_mira <- with(data = imp, exp = lm(chl ~ age + bmi))
+
+test_that("r.squared", {
+    result <- as.vector(pool.r.squared(fit_mira, adjusted = FALSE)[1, ])
+    truth <- c(0.529660990363042, 0.155185078144953, 0.795625721175786, NaN)
+    expect_equal(result, truth)
+})
+
+test_that("r.squared.adjusted", {
+    result <- as.vector(pool.r.squared(fit_mira, adjusted = TRUE)[1, ])
+    truth <- c(0.486574783461685, 0.111808390236973, 0.774997055801878, NaN)
+    expect_equal(result, truth)
+})
+
