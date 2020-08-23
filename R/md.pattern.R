81c5d999084690dcb87078d07602ba67d4e1ea8b
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-08-23 10:31:17 +0200

    Style l-m

--- R/md.pattern.R
+++ R/md.pattern.R
@@ -1,116 +1,118 @@
-# ------------------------------MD.PATTERN-------------------------------
-
-#'Missing data pattern
+#' Missing data pattern
 #'
-#'Display missing-data patterns.
+#' Display missing-data patterns.
 #'
-#'This function is useful for investigating any structure of missing
-#'observations in the data. In specific case, the missing data pattern could be
-#'(nearly) monotone. Monotonicity can be used to simplify the imputation model.
-#'See Schafer (1997) for details. Also, the missing pattern could suggest which
-#'variables could potentially be useful for imputation of missing entries.
+#' This function is useful for investigating any structure of missing
+#' observations in the data. In specific case, the missing data pattern could be
+#' (nearly) monotone. Monotonicity can be used to simplify the imputation model.
+#' See Schafer (1997) for details. Also, the missing pattern could suggest which
+#' variables could potentially be useful for imputation of missing entries.
 #'
-#'@param x A data frame or a matrix containing the incomplete data.  Missing
-#'values are coded as NA's.
-#'@param plot Should the missing data pattern be made into a plot. Default is 
-#'`plot = TRUE`.
-#'@param rotate.names Whether the variable names in the plot should be placed 
-#'horizontally or vertically. Default is `rotate.names = FALSE`.
-#'@return A matrix with \code{ncol(x)+1} columns, in which each row corresponds
-#'to a missing data pattern (1=observed, 0=missing).  Rows and columns are
-#'sorted in increasing amounts of missing information. The last column and row
-#'contain row and column counts, respectively.
-#'@author Gerko Vink, 2018, based on an earlier version of the same function by
-#'Stef van Buuren, Karin Groothuis-Oudshoorn, 2000
-#'@references Schafer, J.L. (1997), Analysis of multivariate incomplete data.
-#'London: Chapman&Hall.
+#' @param x A data frame or a matrix containing the incomplete data.  Missing
+#' values are coded as NA's.
+#' @param plot Should the missing data pattern be made into a plot. Default is
+#' `plot = TRUE`.
+#' @param rotate.names Whether the variable names in the plot should be placed
+#' horizontally or vertically. Default is `rotate.names = FALSE`.
+#' @return A matrix with \code{ncol(x)+1} columns, in which each row corresponds
+#' to a missing data pattern (1=observed, 0=missing).  Rows and columns are
+#' sorted in increasing amounts of missing information. The last column and row
+#' contain row and column counts, respectively.
+#' @author Gerko Vink, 2018, based on an earlier version of the same function by
+#' Stef van Buuren, Karin Groothuis-Oudshoorn, 2000
+#' @references Schafer, J.L. (1997), Analysis of multivariate incomplete data.
+#' London: Chapman&Hall.
 #'
-#'Van Buuren, S., Groothuis-Oudshoorn, K. (2011). \code{mice}: Multivariate
-#'Imputation by Chained Equations in \code{R}. \emph{Journal of Statistical
-#'Software}, \bold{45}(3), 1-67. \url{https://www.jstatsoft.org/v45/i03/}
-#'@keywords univar
-#'@examples
-#'
-#'
-#'md.pattern(nhanes)
-#'#     age hyp bmi chl
-#'#  13   1   1   1   1  0
-#'#   1   1   1   0   1  1
-#'#   3   1   1   1   0  1
-#'#   1   1   0   0   1  2
-#'#   7   1   0   0   0  3
-#'#   0   8   9  10 27
-#'
-#'
-#'@export
-md.pattern <- function(x, plot = TRUE, rotate.names = FALSE){
-  if (!(is.matrix(x) || is.data.frame(x))) 
+#' Van Buuren, S., Groothuis-Oudshoorn, K. (2011). \code{mice}: Multivariate
+#' Imputation by Chained Equations in \code{R}. \emph{Journal of Statistical
+#' Software}, \bold{45}(3), 1-67. \url{https://www.jstatsoft.org/v45/i03/}
+#' @keywords univar
+#' @examples
+#' md.pattern(nhanes)
+#' #     age hyp bmi chl
+#' #  13   1   1   1   1  0
+#' #   1   1   1   0   1  1
+#' #   3   1   1   1   0  1
+#' #   1   1   0   0   1  2
+#' #   7   1   0   0   0  3
+#' #   0   8   9  10 27
+#' @export
+md.pattern <- function(x, plot = TRUE, rotate.names = FALSE) {
+  if (!(is.matrix(x) || is.data.frame(x))) {
     stop("Data should be a matrix or dataframe")
-  if (ncol(x) < 2) 
+  }
+  if (ncol(x) < 2) {
     stop("Data should have at least two columns")
+  }
   R <- is.na(x)
   nmis <- colSums(R)
-  R <- matrix(R[, order(nmis)], dim(x)) #sort columnwise
-  pat <- apply(R, 1, function(x) paste(as.numeric(x), collapse=''))
-  sortR <- matrix(R[order(pat), ], dim(x)) #sort rowwise
-  if (nrow(x) == 1){
+  # sort columnwise
+  R <- matrix(R[, order(nmis)], dim(x))
+  pat <- apply(R, 1, function(x) paste(as.numeric(x), collapse = ""))
+  # sort rowwise
+  sortR <- matrix(R[order(pat), ], dim(x))
+  if (nrow(x) == 1) {
     mpat <- is.na(x)
   } else {
     mpat <- sortR[!duplicated(sortR), ]
   }
-  #update row and column margins
-  if (all(!is.na(x))){
-    cat(" /\\     /\\\n{  `---'  }\n{  O   O  }\n==>  V <==") 
+  # update row and column margins
+  if (all(!is.na(x))) {
+    cat(" /\\     /\\\n{  `---'  }\n{  O   O  }\n==>  V <==")
     cat("  No need for mice. This data set is completely observed.\n")
     cat(" \\  \\|/  /\n  `-----'\n\n")
     mpat <- t(as.matrix(mpat, byrow = TRUE))
     rownames(mpat) <- table(pat)
   } else {
-    if(is.null(dim(mpat))){
+    if (is.null(dim(mpat))) {
       mpat <- t(as.matrix(mpat))
     }
     rownames(mpat) <- table(pat)
   }
   r <- cbind(abs(mpat - 1), rowSums(mpat))
   r <- rbind(r, c(nmis[order(nmis)], sum(nmis)))
-  if (plot){ #add plot
+  if (plot) {
     plot.new()
-    if (is.null(dim(sortR[!duplicated(sortR), ]))){
-      R <- t(as.matrix(r[1:nrow(r)-1, 1:ncol(r)-1]))
+    if (is.null(dim(sortR[!duplicated(sortR), ]))) {
+      R <- t(as.matrix(r[1:nrow(r) - 1, 1:ncol(r) - 1]))
     } else {
-      if(is.null(dim(R))){
+      if (is.null(dim(R))) {
         R <- t(as.matrix(R))
       }
-      R <- r[1:nrow(r)-1, 1:ncol(r)-1]
+      R <- r[1:nrow(r) - 1, 1:ncol(r) - 1]
     }
     op <- par(mar = rep(0, 4))
     on.exit(par(op))
     if (rotate.names) {
-      adj = c(0, 0.5)
-      srt = 90
-      length_of_longest_colname = max(nchar(colnames(r)))/2.6
-      plot.window(xlim = c(-1, ncol(R) + 1), 
-                  ylim = c(-1, nrow(R) + length_of_longest_colname), 
-                  asp = 1)
+      adj <- c(0, 0.5)
+      srt <- 90
+      length_of_longest_colname <- max(nchar(colnames(r))) / 2.6
+      plot.window(
+        xlim = c(-1, ncol(R) + 1),
+        ylim = c(-1, nrow(R) + length_of_longest_colname),
+        asp = 1
+      )
     } else {
-      adj = c(0.5, 0)
-      srt = 0
-      plot.window(xlim = c(-1, ncol(R) + 1), 
-                  ylim = c(-1, nrow(R) + 1), 
-                  asp = 1)
+      adj <- c(0.5, 0)
+      srt <- 0
+      plot.window(
+        xlim = c(-1, ncol(R) + 1),
+        ylim = c(-1, nrow(R) + 1),
+        asp = 1
+      )
     }
     M <- cbind(c(row(R)), c(col(R))) - 1
     shade <- ifelse(R[nrow(R):1, ], mdc(1), mdc(2))
     rect(M[, 2], M[, 1], M[, 2] + 1, M[, 1] + 1, col = shade)
-    for(i in 1:ncol(R)) {
+    for (i in 1:ncol(R)) {
       text(i - .5, nrow(R) + .3, colnames(r)[i], adj = adj, srt = srt)
       text(i - .5, -.3, nmis[order(nmis)][i])
     }
-    for(i in 1:nrow(R)){
-      text(ncol(R) + .3, i - .5, r[(nrow(r)-1):1, ncol(r)][i], adj = 0)
-      text(-.3, i - .5, rownames(r)[(nrow(r)-1):1][i], adj = 1)
+    for (i in 1:nrow(R)) {
+      text(ncol(R) + .3, i - .5, r[(nrow(r) - 1):1, ncol(r)][i], adj = 0)
+      text(-.3, i - .5, rownames(r)[(nrow(r) - 1):1][i], adj = 1)
     }
-    text(ncol(R) + .3,  -.3, r[nrow(r), ncol(r)])
+    text(ncol(R) + .3, -.3, r[nrow(r), ncol(r)])
     return(r)
   } else {
     return(r)
--- R/method.R
+++ R/method.R
@@ -1,7 +1,7 @@
 #' Creates a \code{method} argument
 #'
-#' This helper function creates a valid \code{method} vector. The 
-#' \code{method} vector is an argument to the \code{mice} function that 
+#' This helper function creates a valid \code{method} vector. The
+#' \code{method} vector is an argument to the \code{mice} function that
 #' specifies the method for each block.
 #' @inheritParams mice
 #' @return Vector of \code{length(blocks)} element with method names
@@ -9,20 +9,10 @@
 #' @examples
 #' make.method(nhanes2)
 #' @export
-make.method <- function(data, where = make.where(data), blocks = make.blocks(data),
+make.method <- function(data,
+                        where = make.where(data),
+                        blocks = make.blocks(data),
                         defaultMethod = c("pmm", "logreg", "polyreg", "polr")) {
-  
-  assign.method <- function(y) {
-    if (is.numeric(y)) return(1)
-    if (nlevels(y) == 2) return(2)
-    if (is.ordered(y) && nlevels(y) > 2) return(4)
-    if (nlevels(y) > 2) return(3)
-    if (is.logical(y)) return(2)
-    return(1)
-  }
-  
-  # assign methods based on type, 
-  # use method 1 if there is no single method within the block
   method <- rep("", length(blocks))
   names(method) <- names(blocks)
   for (j in names(blocks)) {
@@ -37,30 +27,36 @@ make.method <- function(data, where = make.where(data), blocks = make.blocks(dat
   method
 }
 
-check.method <- function(method, data, where, blocks, defaultMethod) {
 
-  if (is.null(method)) return(make.method(data = data, 
-                                          where = where,
-                                          blocks = blocks,
-                                          defaultMethod = defaultMethod))
+check.method <- function(method, data, where, blocks, defaultMethod) {
+  if (is.null(method)) {
+    return(make.method(
+      data = data,
+      where = where,
+      blocks = blocks,
+      defaultMethod = defaultMethod
+    ))
+  }
   nimp <- nimp(where, blocks)
 
   # expand user's imputation method to all visited columns
   # single string supplied by user (implicit assumption of two columns)
   if (length(method) == 1) {
-    if (is.passive(method))
+    if (is.passive(method)) {
       stop("Cannot have a passive imputation method for every column.")
+    }
     method <- rep(method, length(blocks))
     method[nimp == 0] <- ""
   }
-  
+
   # check the length of the argument
-  if (length(method) != length(blocks))
+  if (length(method) != length(blocks)) {
     stop("Length of method differs from number of blocks", call. = FALSE)
-  
+  }
+
   # add names to method
   names(method) <- names(blocks)
-  
+
   # check whether the requested imputation methods are on the search path
   active.check <- !is.passive(method) & nimp > 0 & method != ""
   passive.check <- is.passive(method) & nimp > 0 & method != ""
@@ -77,34 +73,68 @@ check.method <- function(method, data, where, blocks, defaultMethod) {
     vname <- blocks[[j]]
     y <- data[, vname, drop = FALSE]
     mj <- method[j]
-    mlist <- list(m1 = c("logreg", "logreg.boot", "polyreg", "lda", "polr"), 
-                  m2 = c("norm", "norm.nob", "norm.predict", "norm.boot",
-                         "mean", "2l.norm", "2l.pan",
-                         "2lonly.norm", "2lonly.pan", 
-                         "quadratic", "ri"), 
-                  m3 = c("norm", "norm.nob", "norm.predict", "norm.boot",
-                         "mean", "2l.norm", "2l.pan", 
-                         "2lonly.norm", "2lonly.pan", 
-                         "quadratic", "logreg", "logreg.boot"))
+    mlist <- list(
+      m1 = c("logreg", "logreg.boot", "polyreg", "lda", "polr"),
+      m2 = c(
+        "norm", "norm.nob", "norm.predict", "norm.boot",
+        "mean", "2l.norm", "2l.pan",
+        "2lonly.norm", "2lonly.pan",
+        "quadratic", "ri"
+      ),
+      m3 = c(
+        "norm", "norm.nob", "norm.predict", "norm.boot",
+        "mean", "2l.norm", "2l.pan",
+        "2lonly.norm", "2lonly.pan",
+        "quadratic", "logreg", "logreg.boot"
+      )
+    )
     cond1 <- sapply(y, is.numeric)
-    cond2 <- sapply(y, is.factor) & sapply(y, nlevels) == 2 
+    cond2 <- sapply(y, is.factor) & sapply(y, nlevels) == 2
     cond3 <- sapply(y, is.factor) & sapply(y, nlevels) > 2
-    if (any(cond1) && mj %in% mlist$m1)
-      warning("Type mismatch for variable(s): ", 
-              paste(vname[cond1], collapse = ", "),
-              "\nImputation method ", mj, " is for categorical data.",
-              call. = FALSE)
-    if (any(cond2) && mj %in% mlist$m2)
-      warning("Type mismatch for variable(s): ", 
-              paste(vname[cond2], collapse = ", "),
-              "\nImputation method ", mj, " is not for factors.", 
-              call. = FALSE)
-    if (any(cond3) && mj %in% mlist$m3)
-      warning("Type mismatch for variable(s): ", 
-              paste(vname[cond3], collapse = ", "),
-              "\nImputation method ", mj, " is not for factors with >2 levels.",
-              call. = FALSE)
+    if (any(cond1) && mj %in% mlist$m1) {
+      warning("Type mismatch for variable(s): ",
+        paste(vname[cond1], collapse = ", "),
+        "\nImputation method ", mj, " is for categorical data.",
+        call. = FALSE
+      )
+    }
+    if (any(cond2) && mj %in% mlist$m2) {
+      warning("Type mismatch for variable(s): ",
+        paste(vname[cond2], collapse = ", "),
+        "\nImputation method ", mj, " is not for factors.",
+        call. = FALSE
+      )
+    }
+    if (any(cond3) && mj %in% mlist$m3) {
+      warning("Type mismatch for variable(s): ",
+        paste(vname[cond3], collapse = ", "),
+        "\nImputation method ", mj, " is not for factors with >2 levels.",
+        call. = FALSE
+      )
+    }
   }
   method[nimp == 0] <- ""
   unlist(method)
+}
+
+
+# assign methods based on type,
+# use method 1 if there is no single method within the block
+assign.method <- function(y) {
+  if (is.numeric(y)) {
+    return(1)
+  }
+  if (nlevels(y) == 2) {
+    return(2)
+  }
+  if (is.ordered(y) && nlevels(y) > 2) {
+    return(4)
+  }
+  if (nlevels(y) > 2) {
+    return(3)
+  }
+  if (is.logical(y)) {
+    return(2)
+  }
+  1
 }
