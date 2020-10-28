449c9e3150a3b3a60e75797a0163b004a071e5ca
Stef van Buuren <stef.vanbuuren@tno.nl>
2020-10-28 23:32:53 +0100

    Improve getfit() documentation

--- R/getfit.R
+++ R/getfit.R
@@ -1,25 +1,32 @@
-#' Extract list of fitted model
+#' Extract list of fitted models
 #'
-#' \code{getfit} returns the list of objects containing the repeated analysis
-#' results, or optionally, one of these fit objects.
+#' Function \code{getfit()} returns the list of objects containing the repeated analysis
+#' results, or optionally, one of these fitted objects. The function looks for
+#' a list element called \code{analyses}, and return this component as a list with
+#' \code{mira} class. If element \code{analyses} is not found in \code{x}, then
+#' it returns \code{x} as a \code{mira} object.
 #'
-#' @param x An object of class \code{mira} or \code{mitml.result},
-#' typically produced by a call to \code{with()}.
-#' @param i An integer between 1 and \code{x$m} signaling the number of the
+#' No checking is done for validity of objects. The function also processes
+#' objects of class \code{mitml.result} from the \code{mitml} package.
+#'
+#' @param x An object of class \code{mira}, typically produced by a call
+#' to \code{with()}.
+#' @param i An integer between 1 and \code{x$m} signalling the index of the
 #' repeated analysis. The default \code{i= -1} return a list with all analyses.
 #' @param simplify Should the return value be unlisted?
-#' @return If \code{i = -1} an object of class \code{mitml.result} containing
-#' all analyses, otherwise it returns the fitted object of
-#' the i'th repeated analysis.
-#' @author Stef van Buuren, March 2012.
+#' @return If \code{i = -1} an object of class \code{mira} containing
+#' all analyses. If \code{i} selects one of the analyses, then it return
+#' an object whose with class inherited from that element.
+#' @author Stef van Buuren, 2012, 2020
 #' @seealso \code{\link[=mira-class]{mira}}, \code{\link{with.mids}}
 #' @keywords manip
 #' @examples
-#'
-#' imp <- mice(nhanes)
+#' imp <- mice(nhanes, print = FALSE, seed = 21443)
 #' fit <- with(imp, lm(bmi ~ chl + hyp))
-#' getfit(fit)
-#' getfit(fit, 2)
+#' f1 <- getfit(fit)
+#' class(f1)
+#' f2 <- getfit(fit, 2)
+#' class(f2)
 #' @export
 getfit <- function(x, i = -1L, simplify = FALSE) {
   if (is.null(x$analyses)) {
