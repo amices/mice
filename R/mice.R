20f99039a97af27fee3ad22d2d91f5e2b83dde0c
Stef van Buuren <stef.vanbuuren@tno.nl>
2021-12-06 15:31:15 +0100

    Add example code: Fit the imputation model on the training data and apply it to impute the test data

--- R/mice.R
+++ R/mice.R
@@ -281,6 +281,28 @@
 #'
 #' # imputation on mixed data with a different method per column
 #' mice(nhanes2, meth = c("sample", "pmm", "logreg", "norm"))
+#'
+#' \dontrun{
+#' # example where we fit the imputation model on the train data
+#' # and apply the model to impute the test data
+#' set.seed(123)
+#' ignore <- sample(c(TRUE, FALSE), size = 25, replace = TRUE, prob = c(0.3, 0.7))
+#'
+#' # scenario 1: train and test in the same dataset
+#' imp <- mice(nhanes2, m = 2, ignore = ignore, print = FALSE, seed = 22112)
+#' imp.test1 <- filter(imp, ignore)
+#' imp.test1$data
+#' complete(imp.test1, 1)
+#' complete(imp.test1, 2)
+#'
+#' # scenario 2: train and test in separate datasets
+#' traindata <- nhanes2[!ignore, ]
+#' testdata <- nhanes2[ignore, ]
+#' imp.train <- mice(traindata, m = 2, print = FALSE, seed = 22112)
+#' imp.test2 <- mice.mids(imp.train, newdata = testdata)
+#' complete(imp.test2, 1)
+#' complete(imp.test2, 2)
+#' }
 #' @export
 mice <- function(data,
                  m = 5,
