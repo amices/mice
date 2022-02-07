f59110de2afb8e08a76ad170761b706a9096f8de
Thom Volker <t.b.volker@uu.nl>
2022-02-07 16:58:52 +0100

    Update documentation futuremice

--- R/futuremice.R
+++ R/futuremice.R
@@ -1,55 +1,57 @@
-#' Wrapper function that runs MICE in futures
+#' Wrapper function that runs MICE in parallel
 #'
 #' This is a wrapper function for \code{\link{mice}}, using multiple cores to
 #' execute \code{\link{mice}} in parallel. As a result, the imputation
-#' procedure can be sped up, which may be useful in general.
+#' procedure can be sped up, which may be useful in general. By default, 
+#' \code{\link{futuremice}} distributes the number of imputations \code{m}
+#' about equally over the cores.
 #'
-#' This function relies on package \code{\link{furrr}}, which is a base
-#' package for R versions 2.14.0 and later. We have chosen to use parallel function
-#' \code{parLapply} to allow the use of \code{parlmice} on Mac, Linux and Windows
-#' systems. For the same reason, we use the Parallel Socket Cluster (PSOCK) type by default.
+#' This function relies on package \code{\link{furrr}}, which is a
+#' package for R versions 3.2.0 and later. We have chosen to use furrr function
+#' \code{future_map} to allow the use of \code{futuremice} on Mac, Linux and Windows
+#' systems.
 #'
-#' On systems other than Windows, it can be hugely beneficial to change the cluster type to
-#' \code{FORK}, as it generally results in improved memory handling. When memory issues
-#' arise on a Windows system, we advise to store the multiply imputed datasets,
-#' clean the memory by using \code{\link{rm}} and \code{\link{gc}} and make another
-#' run using the same settings.
 #'
-#' This wrapper function combines the output of \code{\link{parLapply}} with
+#' This wrapper function combines the output of \code{\link{future_map}} with
 #' function \code{\link{ibind}} in \code{\link{mice}}. A \code{mids} object is returned
 #' and can be used for further analyses.
 #'
-#' Note that if a seed value is desired, the seed should be entered to this function
-#' with argument \code{seed}. Seed values outside the wrapper function (in an
-#' R-script or passed to \code{\link{mice}}) will not result to reproducible results.
-#' We refer to the manual of \code{\link{parallel}} for an explanation on this matter.
+#' A seed value is drawn randomly by default, and accessible through \code{$parallelseed}.
+#' The seed can also be entered through specifying the argument parallelseed in the
+#' \code{\link{futuremice}} call. Hence, results will always be reproducible, regardless
+#' of whether the seed is specified in the global environment, or by setting the same
+#' seed within the function (potentially by extracting the seed from the \code{futuremice}
+#' output object.
 #'
-#' @aliases parlmice
+#' @aliases futuremice
 #' @param data A data frame or matrix containing the incomplete data. Similar to
 #' the first argument of \code{\link{mice}}.
 #' @param m The number of desired imputated datasets. By default $m=5$ as with \code{mice}
+#' @param parallelseed A scalar to be used to obtain reproducible results over the
+#' futures. The default \code{parallelseed = NA} will result in a seed value that is 
+#' randomly drawn between -999999999 and 999999999.
+#' @param n.core A scalar indicating the number of cores that should be used.
 #' @param seed A scalar to be used as the seed value for the mice algorithm within
 #' each parallel stream. Please note that the imputations will be the same for all
 #' streams and, hence, this should be used if and only if \code{n.core = 1} and
 #' if it is desired to obtain the same output as under \code{mice}.
-#' @param n.core A scalar indicating the number of cores that should be used.
-#' @param n.imp.core A scalar indicating the number of imputations per core.
-#' @param cluster.seed A scalar to be used as the seed value. It is recommended to put the
-#' seed value here and not outside this function, as otherwise the parallel processes
-#' will be performed with separate, random seeds.
-#' @param cl.type The cluster type. Default value is \code{"PSOCK"}. Posix machines (linux, Mac)
-#' generally benefit from much faster cluster computation if \code{type} is set to \code{type = "FORK"}.
+#' @param use.logical A logical indicating whether logical (\code{TRUE}) or physical 
+#' (\code{FALSE}) CPU's on machine should be used.
+#' @param future.plan A character indicating how \code{future}s are resolved. 
+#' The default \code{multisession} resolves futures asynchronously (in parallel)
+#' in separate \code{R} sessions running in the background. See \link{\code{plan}}
+#' for more information on future plans.
 #' @param ... Named arguments that are passed down to function \code{\link{mice}} or
 #' \code{\link{makeCluster}}.
 #'
 #' @return A mids object as defined by \code{\link{mids-class}}
 #'
-#' @author Gerko Vink, Rianne Schouten
-#' @seealso \code{\link{parallel}}, \code{\link{parLapply}}, \code{\link{makeCluster}},
-#' \code{\link{mice}}, \code{\link{mids-class}}
+#' @author Thom Benjamin Volker, Gerko Vink
+#' @seealso \code{\link{future}}, \code{\link{furrr}}, \code{\link{future_map}},
+#' \code{\link{plan}}, \code{\link{mice}}, \code{\link{mids-class}}
 #' @references
-#' Schouten, R. and Vink, G. (2017). parlmice: faster, paraleller, micer.
-#' \url{https://www.gerkovink.com/parlMICE/Vignette_parlMICE.html}
+#' Volker, T.B. and Vink, G. (2022). futuremice: The future starts today.
+#' \url{https://www.gerkovink.com/parlMICE/Vignette_futuremice.html}
 #'
 #' #'Van Buuren, S. (2018).
 #' \href{https://stefvanbuuren.name/fimd/parallel-computation.html}{\emph{Flexible Imputation of Missing Data. Second Edition.}}
@@ -58,17 +60,17 @@
 #' @examples
 #' # 150 imputations in dataset nhanes, performed by 3 cores
 #' \dontrun{
-#' imp1 <- parlmice(data = nhanes, n.core = 3, n.imp.core = 50)
+#' imp1 <- futuremice(data = nhanes, m = 150, n.core = 3)
 #' # Making use of arguments in mice.
-#' imp2 <- parlmice(data = nhanes, method = "norm.nob", m = 100)
+#' imp2 <- futuremice(data = nhanes, m = 100, method = "norm.nob")
 #' imp2$method
 #' fit <- with(imp2, lm(bmi ~ hyp))
 #' pool(fit)
 #' }
 #'
 #' @export
-futuremice <- function(data, m = 5, seed = NA, parallelseed = NA, n.core = NULL,
-                       n.imp.core = NULL, use.logical = TRUE, ...) {
+futuremice <- function(data, m = 5, parallelseed = NA, n.core = NULL, seed = NA,
+                       use.logical = TRUE, future.plan = "multisession", ...) {
   
   # check if pacakages available
   mice:::install.on.demand("parallelly", ...)
@@ -77,7 +79,7 @@ futuremice <- function(data, m = 5, seed = NA, parallelseed = NA, n.core = NULL,
   
   # check form of data and m
   data <- check.dataform(data)
-  m <- check.m(m)
+  m    <- check.m(m)
 
   # check if data complete
   if (sum(is.na(data)) == 0) {
@@ -87,41 +89,52 @@ futuremice <- function(data, m = 5, seed = NA, parallelseed = NA, n.core = NULL,
   # number of available cores
   available <- parallelly::availableCores(logical = use.logical)
   
-  # check if arguments match CPU specifications
   if (!is.null(n.core)) {
+    # check if arguments match CPU specifications
     if (n.core > available) {
-      stop("Number of cores specified is greater than the number of logical cores in your CPU")
+      warning(paste("Number of cores specified is greater than the number of logical cores in your CPU, futuremice falls back to", available, "cores."))
+      n.core <- available
+    } 
+    if (n.core > m) {
+      message(paste("The number of cores exceeds the number of imputations. The number of cores used is set equal to the number of imputations (m = ", m, ")."))
+      n.core <- m
     }
+  } else {
+    n.core <- available - 1
+    message(paste("Number of cores not specified. Based on your machine a value of n.core =", n.core, "is chosen; the imputations are distributed about equally over the cores."))
   }
-
-  # determine course of action when not all arguments specified
-  if (!is.null(n.core) & is.null(n.imp.core)) {
-    n.imp.core <- m
-    warning(paste("Number of imputations per core not specified: n.imp.core = m =", m, "has been used"))
+  
+  if (n.core > 1) {
+    dist.core <- cut(1:m, n.core, labels = paste0("core", 1:n.core))
+  } else {
+    dist.core <- rep("core1", m) 
   }
-  if (is.null(n.core) & !is.null(n.imp.core)) {
-    n.core <- available - 1 # leave one out by default for other processes. users can override if wanted
-    warning(paste("Number of cores not specified. Based on your machine a value of n.core =", parallelly::availableCores(logical = TRUE) - 1, "is chosen"))
-  }
-  if (is.null(n.core) & is.null(n.imp.core)) {
-    specs <- match.cluster(n.core = (available - 1), m = m)
-    n.core <- specs$cores
-    n.imp.core <- specs$imps
-  }
+  
+  n.imp.core <- as.vector(table(dist.core))
+  
   if (!is.na(seed)) {
     if (n.core > 1) {
-      warning("Be careful; the specified seed is equal for all imputations. Please consider specifying parallelseed instead.")
+      if (interactive()) {
+        msg <- "Be careful; specifying seed rather than parallelseed results in duplicate imputations.\nDo you want to continue?\n"
+        ask <- askYesNo(msg, prompts = getOption("askYesNo", gettext(c("Yes", "No, ignore seed","Cancel"))))
+        
+        if (isTRUE(ask)) {
+          seed <- seed
+          warning("Be careful; the imputations will be the same over the cores.")
+        }
+        else if (isFALSE(ask)) {
+          seed <- NA
+          message("Parallelseed is specified for you, and is accessible in the output object under $parallelseed.")
+        }
+        else if (is.na(ask)) {
+          stop("You stopped futuremice. To obtain unique, but reproducible imputations, specify parallelseed.")
+        }
+      } else {
+        warning("Be careful; the imputations will be identical over the cores. Perhaps you want to specify parallelseed, for unique, but reproducible results.")
+      }
     }
   }
   if (is.na(parallelseed)) {
-    ### Two options here:
-    ### option 1 is to specify the seed as the current random seed
-    # current_seed <- .Random.seed # commented out because I prefer option 2
-    ### and then make the output seed of mice a list, with first the current, regular
-    ### {seed} argument, and second the current state of the random number generator
-    ### as specified above
-    
-    ### option 2 is to randomly sample a random seed, and return this seed
     parallelseed <- round(runif(1, -999999999, 999999999))
   }
   
@@ -129,15 +142,18 @@ futuremice <- function(data, m = 5, seed = NA, parallelseed = NA, n.core = NULL,
   withr::local_seed(parallelseed)
   
   # start multisession
-  future::plan(future::multisession, 
-               workers = n.core) 
+  future::plan(future.plan, 
+               workers = n.core)
   
   # begin future
-  imps <- furrr::future_map(1:n.core, function(x) {
-    mice(data = data, 
-         m = n.imp.core, 
+  imps <- furrr::future_map(n.imp.core, function(x) {
+    
+    mice(data = data,
+         m = x,
          print = F, 
+         seed = seed,
          ...)
+    
   }, .options = furrr::furrr_options(seed = TRUE))
   
   # end multisession
@@ -146,7 +162,7 @@ futuremice <- function(data, m = 5, seed = NA, parallelseed = NA, n.core = NULL,
   # stitch future into mids
   
   # postprocess clustered imputation into a mids object
-  imp <- imps[[1]]
+  imp   <- imps[[1]]
   if (length(imps) > 1) {
     for (i in 2:length(imps)) {
       imp <- ibind(imp, imps[[i]])
@@ -160,16 +176,4 @@ futuremice <- function(data, m = 5, seed = NA, parallelseed = NA, n.core = NULL,
   imp$parallelseed <- parallelseed
 
   return(imp)
-}
-
-match.cluster <- function(n.core, m) {
-  cores <- 1:n.core
-  imps <- 1:m
-  out <- data.frame(
-    results = as.vector(cores %*% t(imps)),
-    cores = cores,
-    imps = rep(imps, each = n.core)
-  )
-  which <- out[out[, "results"] == m, ]
-  which[order(which$cores, decreasing = T), ][1, 2:3]
 }
