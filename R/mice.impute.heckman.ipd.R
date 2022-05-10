fca70b86ab332f94fbb1553c19490b9a5128ab3a
Johanna :D <johamunoza@hotmail.com>
2022-05-10 11:25:23 +0200

    Update with suggestions about inherit
    Inherit changes and specification of suggestion packages

--- R/mice.impute.heckman.ipd.R
+++ R/mice.impute.heckman.ipd.R
@@ -24,7 +24,7 @@
 #' @param pmm for gaussian variables if applies predictive mean matching to
 #' imputed result: "TRUE","FALSE"
 #' @param meta_method meta_analysis estimation method for random effects :
-#' "ml" (maximum likelihood), "reml" (restricted maximul likelihood) or "mm"
+#' "ml" (maximum likelihood), "reml" (restricted maximum likelihood) or "mm"
 #' method of moments.
 #' @param ... Other named arguments. Not used.
 #' @name mice.impute.heckman.ipd
@@ -54,26 +54,27 @@
 
 mice.impute.heckman.ipd <-
   function(y,ry,x,wy = NULL, type, family, full = FALSE, pmm = FALSE, meta_method,...) {
+    install.on.demand("gam", ...)
     install.on.demand("GJRM", ...)
-    install.on.demand("lqmm", ...)
+    install.on.demand("Matrix", ...)
     install.on.demand("mixmeta", ...)
     install.on.demand("mvtnorm", ...)
     install.on.demand("pbivnorm", ...)
-    install.on.demand("gam", ...)
+    
     
     # 1. Define variables and dataset----
     
     # Rename covariates
     colnames(x) <- paste0("x_", 1:length(colnames(x))) #change the covariates name for avoiding conflicts when y is covariate
     bos_name   <-  colnames(x)[type == 1] # names of variables present in both outcome and selection model
-    sel_name    <- colnames(x)[type == -3] # names of variables in selection alone
-    out_name    <- colnames(x)[type == -4] # names of variables in outcome alone
+    sel_name    <- colnames(x)[type == -3] # names of variables in selection model alone
+    out_name    <- colnames(x)[type == -4] # names of variables in outcome model alone
     
     
     # Check if group variable is defined
     if (length(colnames(x)[type == -2]) == 0) { 
-      warning("No clusterized incomplete variable, it will be imputed with no study specific model")
-      Marg_ind <- 0
+      warning("No cluster variable has been provided, a Heckman model will be applied to the whole data set, i.e. without considering the cluster level. ")
+      Marg_ind <- 0 # Indicator that indicates if it is applied 0: Heckman full dataset, 1: Heckman at cluster level
     } else {
       group_name  <- colnames(x)[type == -2]
       names.clust <- as.character(unique(x[, group_name]))
@@ -95,14 +96,14 @@ mice.impute.heckman.ipd <-
     
     # 2. Step 1: Get the theta estimators for each study ----
     
-    if (Marg_ind == 1) {
+    if (Marg_ind == 1) { #Heckman at cluster level
       df_list   <- split.data.frame(data, x[, group_name]) # Separate data frame by group
       res_total <- suppressWarnings( lapply( df_list, copulaIPD,
                                              sel = sel, out = out, send = send, family = family)) #Calculate parameters for each study
       studytype <- sapply(res_total, function(x) x[[2]]) # Specify missing pattern in studies
       sporadical<- studytype[studytype != 0] # List sporadically missing studies
       fit_list  <- lapply(res_total, function(x) x[[1]]) # Get the parameter estimates
-      Marg_ind  <- ifelse(length(fit_list[studytype == 1]) >= 2, 1, 0) # Calculate marginal distribution if there are parameters for at least 2 studies
+      Marg_ind  <- ifelse(length(fit_list[studytype == 1]) >= 2, 1, 0) # Proceed Heckman at cluster level if there are at least 2 clusters with an estimable Heckman model
     }
     
     # 3. Step 2: Get marginal theta and var(theta) ----
@@ -110,7 +111,7 @@ mice.impute.heckman.ipd <-
       coef_list  <- lapply(fit_list[studytype != 0], `[[`, c('coefficients')) # Get effect vector for each study
       Vb_list    <- lapply(fit_list[studytype != 0], `[[`, c('Vb')) # Get covariance matrix for each study
       dcoef_list <- lapply(coef_list[sporadical == 1], function(x) as.data.frame(t(x)))
-      coef_mat_s <- as.matrix(Reduce(rbind,     # this is the same as rbindlist but without calling data.table package
+      coef_mat_s <- as.matrix(Reduce(rbind,     # this do the same that rbindlist but without calling data.table package
                                      Map(function(x) {x[, setdiff(unique(unlist(lapply(dcoef_list, colnames))), names(x))] <- NA
                                      return(x)}, dcoef_list)))
       
@@ -121,25 +122,25 @@ mice.impute.heckman.ipd <-
       
       
       # Draw thetas from the universe of populations
-      if (full == TRUE) {
+      if (full == TRUE) { # use the entire set of parameters 
         total_mar <- draw_theta_psi_mar( coef_mat_s  = coef_mat_s,
                                          Vb_list     = Vb_list[sporadical == 1],
                                          meta_method = meta_method,
                                          Marg_ind   = Marg_ind)
-        Marg_ind <- total_mar[[3]]
+        Marg_ind <- total_mar[[3]] # if marginal parameters where estimated proceed with Heckman cluster estimation otherwise opt for the Heckman full model 
         
         if (Marg_ind == 1) {
           beta_s_mar <- list(total_mar[[1]][selnam], total_mar[[2]][selnam, selnam])
           beta_o_mar <- list(total_mar[[1]][outnam], total_mar[[2]][outnam, outnam])
           rho_t_mar  <- list(total_mar[[1]]["theta.star"], 
                              total_mar[[2]]["theta.star", "theta.star"])
-          if (family != "probit") {
+          if (family == "normal") {
             sigma_t_mar <- list(total_mar[[1]]["sigma.star"], 
                                 total_mar[[2]]["sigma.star", "sigma.star"])
           }
         }
         
-      } else{
+      } else{ # separate the set of parameters in beta_out, beta_s and rho
         beta_s_mar <- draw_theta_psi_mar( coef_mat_s = coef_mat_s,
                                           Vb_list = Vb_list[sporadical == 1],
                                           vnames = selnam,
@@ -169,51 +170,47 @@ mice.impute.heckman.ipd <-
     }
     
     # 4. Get theta_k and var(theta_k) from full conditional distribution ----
-    if (Marg_ind == 1) {
-      for (i in names.clust) {
-        #Loop across studies
+    if (Marg_ind == 1) {  # Applies the Heckman model by cluster
+      for (i in names.clust) {  #Loop across studies
         
-        if (studytype[[i]] == 0) {
-          #systematically missing
+        if (studytype[[i]] == 0) { #systematically missing
           
           beta_s_star <- MASS::mvrnorm(n = 1, mu = beta_s_mar[[1]],Sigma = beta_s_mar[[2]])
           beta_o_star <- MASS::mvrnorm(n = 1, mu = beta_o_mar[[1]],Sigma = beta_o_mar[[2]])
           rho_star <- tanh(MASS::mvrnorm( n = 1, mu = rho_t_mar[[1]],Sigma = rho_t_mar[[2]]))
+          
           if (family == "normal") {
             sigma_star <- exp(MASS::mvrnorm( n = 1, mu = sigma_t_mar[[1]],
                                              Sigma = sigma_t_mar[[2]]))#copula package calls log(sigma) as sigma.star
-          } else {
-            sigma_star <- NA
-          }
+             } else { #binomial
+               sigma_star <- NA
+               }
           
-        } else if (studytype[[i]] == 2) {
-          # Model is only MAR estimable
-          
+        } else if (studytype[[i]] == 2) {  # Only the outcome model is estimable (MAR)
           beta_o_star <- MASS::mvrnorm(n = 1,mu = coef_list[[i]][outnam],Sigma = Vb_list[[i]][outnam, outnam])
           if (family == "normal") {
             sigma_star <- exp(MASS::mvrnorm( n = 1, mu = coef_list[[i]]["sigma.star"],
                                              Sigma = Vb_list[[i]]["sigma.star", "sigma.star"]))#copula package calls log(sigma) as sigma.star
-          } else {
-            sigma_star <- NA
-          }
+             } else { #binomial
+               sigma_star <- NA
+               }
           rho_star <- 0
           
           
-        } else {
-          # sporadically missing
-          
+         } else {# sporadically missing
+       
           beta_s_star <- draw_cond_theta( theta_mar = beta_s_mar,theta_k = coef_list[[i]],
                                           var_theta_k = Vb_list[[i]], vnames = selnam)
           beta_o_star <- draw_cond_theta( theta_mar = beta_o_mar, theta_k = coef_list[[i]],
                                           var_theta_k = Vb_list[[i]], vnames = outnam)
-          if (family != "probit") {
+          if (family == "normal") {
             sigma_star <- exp(draw_cond_theta( theta_mar = sigma_t_mar,
                                                theta_k = coef_list[[i]],
                                                var_theta_k = Vb_list[[i]],
                                                vnames = "sigma.star"))#copula package calls log(sigma) as sigma.star
-          } else{
-            sigma_star <- NA
-          }
+             } else{ # binomial
+               sigma_star <- NA
+               }
           rho_star <- tanh( draw_cond_theta( theta_mar = rho_t_mar,
                                              theta_k = coef_list[[i]],
                                              var_theta_k = Vb_list[[i]], 
@@ -223,9 +220,7 @@ mice.impute.heckman.ipd <-
         
         Xm <- X[!ry & x[, group_name] == as.numeric(i),]
         
-        if (nrow(Xm) != 0) {
-          
-          #Cluster with at least one missing value in outcome equation
+        if (nrow(Xm) != 0) { #Cluster with at least one missing value in outcome equation
           y.star <- gen_y_star( Xm= Xm, sel_name = sel_name, bos_name = bos_name,
                                 out_name =out_name, beta_s_star = beta_s_star, 
                                 beta_o_star = beta_o_star, sigma_star = sigma_star,
@@ -237,14 +232,13 @@ mice.impute.heckman.ipd <-
       }
     }
     
-    if (Marg_ind == 0) {
-      #Full data Heckman model
-      warning(
-        "There is not enough information to impute at the study level, so the imputation is based on the total database without study specification."
-      )
+    if (Marg_ind == 0) { # Applies the Heckman model on entire dataset
+      
+      warning("There is not enough information to impute at the study level, so the imputation is based on the total database without study specification.")
       res <- copulaIPD( data = data, sel = sel, out = out, family = family, send = send)
-      if (!class(res)[[1]] == "copulaSampleSel") {
-        # model is not estimable
+      
+      if (res[[2]] != 1){   # model is not estimable
+       
         stop("There is not enough infomation to impute the Heckman model at marginal or study level")
       }
       
@@ -273,8 +267,10 @@ mice.impute.heckman.ipd <-
 # F 0.1. CopulaIPD: Apply Binomial or Gaussian model depending on y type
 
 copulaIPD <- function(data, sel, out, family, send) {
-  family<-ifelse(family=="probit","probit","N")
-  fit_ind <- 0  # No estimable
+  family <- ifelse(family=="probit","probit","N") # According to GJRM family specfication 
+  fit_ind <- 0 # None model estimable for the cluster
+  
+  # A. Estimate Heckman model 
   fit <- try(GJRM::gjrm( formula = list(sel, out),
                          data = data,
                          margins = c("probit", family),
@@ -284,7 +280,7 @@ copulaIPD <- function(data, sel, out, family, send) {
                          parscale = TRUE),
              silent = TRUE)
   
-  if (!any(class(fit) == "try-error")) {
+  if (!any(inherits(fit, "try-error"))) {
     # model is estimable
     ev <- eigen(fit$fit$hessian, symmetric = TRUE, only.values = TRUE)$values
     convh <- min(ev) > 0  # convergence based on hessian positive definiteness
@@ -292,7 +288,7 @@ copulaIPD <- function(data, sel, out, family, send) {
     
     if (convh & convg) {
       # MNAR estimable
-      fit_ind <- 1
+      fit_ind <- 1 # Heckman model estimable for the cluster
       names <- c(paste0(names(fit$coefficients)[1:send], "_s"), 
                  names(fit$coefficients[(send + 1):length(names(fit$coefficients))]))
       names(fit$coefficients) <- names
@@ -301,13 +297,14 @@ copulaIPD <- function(data, sel, out, family, send) {
     }
   }
   
-  if (fit_ind != 1) {
+  #B. If Heckamn is not estimable, estimate outcome model alone
+  if (fit_ind == 0) { 
     fit <- try(gam::gam(data = data, formula = out,
-                        family = ifelse(family == "probit","binomial","gaussian")),
+                        family = ifelse(family == "probit","binomial","gaussian")), # According to gam family specfication 
                silent = TRUE)
     
     if (!any(is.na(fit$coefficients))) {
-      fit_ind <- 2 # MAR estimable.
+      fit_ind <- 2 # Only outcome model estimable for the cluster
       fit$Vb  <- stats::vcov(fit)
       
       if (family != "probit") {
@@ -316,14 +313,12 @@ copulaIPD <- function(data, sel, out, family, send) {
         fit$Vb <- rbind(fit$Vb, sigma.star = c(rep(0, nrow(fit$Vb)),
                                                2 / (fit$df.residual - 4)))
       }
+    }else{ # not enough info to fit any model fit_ind = 0
+      fit <- NA
     }
   }
   
-  if (fit_ind == 0) {
-    # not enough info to fit any model
-    fit <- NA
-    fit_ind <- 0
-  }
+ 
   return(list(fit, fit_ind))
   
 }
@@ -344,8 +339,8 @@ draw_theta_psi_mar <- function(coef_mat_s, Vb_list, meta_method, vnames = NULL, 
   theta_star <- NA
   psi_star <- NA
   
-  if (is.null(vnames)) {
-    vnames <- colnames(coef_mat_s) #use all set of parameters
+  if (is.null(vnames)) { #use all set of parameters
+    vnames <- colnames(coef_mat_s) 
   }
   
   # Get covariance matrix
@@ -358,14 +353,14 @@ draw_theta_psi_mar <- function(coef_mat_s, Vb_list, meta_method, vnames = NULL, 
     mvma <- try(mixmeta::mixmeta(coef_mat_s,cov_mat_s, method = meta_method,
                                  control = list(hessian = TRUE)), silent = TRUE)
     
-    if (class(mvma) == "try-error") {
-      # Use mm instead
+  
+    if (inherits(mvma,"try-error")) {  # Use mm instead
       meta_method = "mm"
       mvma <- try(mixmeta::mixmeta(coef_mat_s, cov_mat_s, method = meta_method, 
                                    control = list(hessian = TRUE)),silent = TRUE)
       
-      if (class(mvma) == "try-error") {
-        # MA can not be estimated
+    
+      if (inherits(mvma,"try-error")) {  # MA can not be estimated
         Marg_ind <- 0
       }
     }
@@ -393,8 +388,8 @@ draw_theta_psi_mar <- function(coef_mat_s, Vb_list, meta_method, vnames = NULL, 
         psi_star <- Matrix::tcrossprod(psi)
       }
       
-    } else
-      # meta_method== reml OR ml
+    } else  # meta_method== reml OR ml
+     
       psi_star <- mvma$Psi
   }
   
@@ -419,19 +414,18 @@ gen_y_star <- function(Xm, sel_name, bos_name, out_name, beta_s_star, beta_o_sta
   
   XOBO <- data.matrix(Xm[,colnames(Xm) %in% c("Int",bos_name,out_name)]) %*% as.vector(beta_o_star)
   
-  if (studytype != 2) {
+  if (studytype != 2) { #No MAR
     XSBS <- data.matrix(Xm[,colnames(Xm) %in% c("Int",sel_name,bos_name)]) %*% as.vector(beta_s_star)
   }
   
-  if (family == "normal") {
-    if (studytype != 2) {
-      # Not MAR
+  if (family == "normal") { # normal missing variable
+    if (studytype != 2) {    # Heckman
       Ratio <- (-stats::dnorm(XSBS) / (stats::pnorm(-XSBS)))
       Ratio[is.na(Ratio) | is.infinite(Ratio)] <- 0.0
       y.star <- XOBO + as.numeric(sigma_star) * as.numeric(rho_star) * Ratio + 
         rnorm(nrow(XSBS), 0, sd = sigma_star)
       
-    } else {
+    } else { # MAR
       y.star <- XOBO + rnorm(nrow(XOBO), 0, sd = sigma_star)
     }
     
@@ -440,16 +434,16 @@ gen_y_star <- function(Xm, sel_name, bos_name, out_name, beta_s_star, beta_o_sta
       y.star <- y[ry == 1][idx]
     }
     
-  } else {
-    if (studytype != 2) {
-      # Not MAR
+  } else { #binomial missing varible
+    if (studytype != 2) { # Heckman
+  
       p.star <- pbivnorm::pbivnorm(as.vector(XOBO),-as.vector(XSBS),
                                    -as.numeric(rho_star)) / stats::pnorm(-XSBS)
       p.star[is.na(p.star) | (is.infinite(p.star) & p.star < 0) |p.star < 0.0 | 
                p.star == "0"] <- 0.0
       p.star[p.star > 1.0 | p.star == "1" |(is.infinite(p.star) & p.star > 0)] <- 1.0
       
-    } else {
+    } else { # MAR
       expXOBO <- exp(XOBO)
       p.star <- expXOBO / (1 + expXOBO)
       p.star[(is.infinite(expXOBO) & expXOBO < 0)] <- 0
