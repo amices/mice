3bc1beee9eef6b66c06276d26148bd2c2e5bc72a
Vladimir Khodygo <vladimir.khodygo@glasgow.ac.uk>
2023-07-20 15:58:03 +0100

    DOC: fix typos

--- R/futuremice.R
+++ R/futuremice.R
@@ -80,7 +80,7 @@
 futuremice <- function(data, m = 5, parallelseed = NA, n.core = NULL, seed = NA,
                        use.logical = TRUE, future.plan = "multisession", 
                        packages = NULL, globals = NULL, ...) {
-  # check if pacakages available
+  # check if packages available
   install.on.demand("parallelly", ...)
   install.on.demand("furrr", ...)
   install.on.demand("future", ...)
