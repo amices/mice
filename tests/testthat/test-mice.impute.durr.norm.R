aaaed49a9da735fe99c9341754a996d85831cee7
Stef van Buuren <stef.vanbuuren@tno.nl>
2023-09-03 20:26:53 +0200

    Add test for issue #577

--- tests/testthat/test-mice.impute.durr.norm.R
+++ tests/testthat/test-mice.impute.durr.norm.R
@@ -31,13 +31,13 @@ test_that("Returns requested length", {
 
 boys_cont <- boys[, 1:4]
 durr_default <- mice(boys_cont,
-  m = 2, maxit = 2, method = "lasso.norm",
-  print = FALSE
+                     m = 2, maxit = 2, method = "lasso.norm",
+                     print = FALSE
 )
 durr_custom <- mice(boys_cont,
-  m = 2, maxit = 2, method = "lasso.norm",
-  nfolds = 5,
-  print = FALSE
+                    m = 2, maxit = 2, method = "lasso.norm",
+                    nfolds = 5,
+                    print = FALSE
 )
 
 test_that("mice call works", {
@@ -47,3 +47,31 @@ test_that("mice call works", {
 test_that("mice call works w/ custom arguments", {
   expect_equal(class(durr_custom), "mids")
 })
+
+#########################
+# TEST 3: Failure because removals by remove.lindep()  #577
+#########################
+
+# library(mice, warn.conflicts = FALSE)
+# set.seed(123)
+#
+# n <- 100
+# y <- rnorm(n)
+# x <- rep(1, n)
+# y[sample(1:n, n * .3)] <- NA
+# ry <- !is.na(y)
+#
+# # Test univariate imputation outside mice
+# imps <- mice.impute.lasso.norm(y, ry, as.matrix(x))
+# imps <- mice.impute.lasso.norm(y, ry, as.matrix(x)[, -1])
+#
+# # Test inside mice
+# input <- data.frame(y = y, x = 1)
+# imp <- mice(input, m = 1, maxit = 1, method = "lasso.norm", print = FALSE)
+# imp <- mice(input, m = 1, maxit = 1, method = "lasso.norm", print = FALSE, eps = 0)
+# imp <- mice(input, m = 1, maxit = 1, method = "lasso.norm", print = FALSE, eps = 0, remove.constant = FALSE)
+#
+# test_that("skip remove.lindep()", {
+#   expect_silent(mice(data, m = 1, maxit = 1, method = "lasso.norm", eps = 0, print = FALSE))
+# })
+#
