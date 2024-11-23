f16f38d4705fb4cb203c62edeb740971c9a93b59
Stef van Buuren <stef.vanbuuren@tno.nl>
2024-11-23 17:56:12 +0100

    Move glmnet to Suggests:

--- R/imports.R
+++ R/imports.R
@@ -2,7 +2,6 @@
 #' @importFrom broom    glance tidy
 #' @importFrom dplyr    %>% any_of bind_cols bind_rows filter group_by lead
 #'                      mutate n pull relocate row_number select summarize
-#' @importFrom glmnet   cv.glmnet
 #' @importFrom graphics abline axis box par plot plot.new plot.window
 #'                      points rect text
 #' @importFrom lattice  bwplot densityplot stripplot xyplot
