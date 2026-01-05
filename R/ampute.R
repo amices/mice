80700849fbe578bf4462b424db9a77a1a59d5c72
Volker <t.b.volker@uu.nl>
2026-01-05 12:03:58 +0100

    Remove outcommented line

--- R/ampute.R
+++ R/ampute.R
@@ -208,7 +208,6 @@ ampute <- function(data, prop = 0.5, patterns = NULL, freq = NULL,
   if (is.null(data)) {
     stop("Argument data is missing, with no default", call. = FALSE)
   }
-  # data.in <- data # preserve an original set to inject the NA's in later
   data <- check.dataform(data)
   if (anyNA(data)) {
     stop("Data cannot contain NAs", call. = FALSE)
