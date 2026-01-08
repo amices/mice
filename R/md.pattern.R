8ee181bd0c75ae842b9f64621814f0f38b80e58b
MattxFive <132918080+MattxFive@users.noreply.github.com>
2026-01-08 16:17:21 +0100

    Update md.pattern.R
    Addition of several parameters to allow further plot customisation. Using the function without any specification displays the same plot.
    
    @rotate.names : allows a numeric value
    @pattern.ord : allows to sort the plot according to the number of missing variables
    @min.ind : allows to drop observations below a minimum of individuals
    @nb.pat : allows to drop the number of variables composing the pattern - right numbers
    @nb.var : allows to drop the number of individuals per variable missing - bottom numbers. Also automatically drops bottom-right corner total missing values.
    @nb.ind : allows to drop the number of individuals composing the pattern - left numbers
    @nb.tot : allows to drop the total number of missing values - bottom right corner. Automatically dropped by nb.var = FALSE
    @names : allows to provide a custom vector of names to be displayed at the top of the plot
    @colors : allows to choose colors to be displayed for missing and non-missing values
    
    Line 56 : function updated with new parameters
    Lines 63-65 : check consistency between ncol(x) and length of the @names, especially if a vector is specified
    Lines 66-68 : set nb.tot = FALSE if nb.var = FALSE
    Line 70 : allocate @names
    Lines 95-97 : drops rows below the minimum of observations specified if @min.ind if specified
    Lines 99-101 : sort the matrix according to the number of variables in the patterns if @pattern.ord is specified
    Lines 115-121 : adj and the plot parameters are modified to fit the numeric values of @rotate.names
    Line 123 : @colors parameters used instead of static colors
    Lines 127-129 : displays nb.var if TRUE
    Lines 132-134 : displays nb.pat if TRUE
    Lines 135-137 : displays nb.ind if TRUE
    Lines 139-141 : displays nb.tot if TRUE

--- R/md.pattern.R
+++ R/md.pattern.R
@@ -8,18 +8,34 @@
 #' See Schafer (1997) for details. Also, the missing pattern could suggest which
 #' variables could potentially be useful for imputation of missing entries.
 #'
-#' @param x A data frame or a matrix containing the incomplete data.  Missing
+#' @param x A data frame or a matrix containing the incomplete data. Missing
 #' values are coded as NA's.
 #' @param plot Should the missing data pattern be made into a plot. Default is
 #' `plot = TRUE`.
-#' @param rotate.names Whether the variable names in the plot should be placed
-#' horizontally or vertically. Default is `rotate.names = FALSE`.
+#' @param rotate.names Rotation of variables names. Default is `rotate.names = 0`.
+#' @param pattern.ord Should patterns be presented by increasing order ? Then `nb.pat`
+#' is sorted. Default is `pattern.ord = FALSE`.
+#' @param min.ind The minimum number of individuals for a pattern to be displayed on the plot.
+#' Default is NULL.
+#' @param nb.pat Should number of variables per pattern be displayed on the
+#' right side of the graph ? Default is `nb.pat = TRUE`.
+#' @param nb.var Should number of individuals presenting missing values per
+#' variable be displayed at the bottom of the graph ? Automatically sets
+#' `nb.tot = FALSE`. Default is `nb.var = TRUE`.
+#' @param nb.ind Should number of individuals per pattern be displayed at the
+#' left of the graph ? Default is `nb.ind = TRUE`.
+#' @param nb.tot Should total number of missing values
+#' be displayed at the bottom-right corner of the graph ? Default is `nb.tot = TRUE`.
+#' @param names A char vector with customized colnames. Default is `names = colnames(x)`.
+#' @param colors Specify colors you want to display. Default is
+#' `colors = c("#006CC2B3", "#B61A51B3")`.
+
 #' @return A matrix with \code{ncol(x)+1} columns, in which each row corresponds
-#' to a missing data pattern (1=observed, 0=missing).  Rows and columns are
+#' to a missing data pattern (1=observed, 0=missing). Rows and columns are
 #' sorted in increasing amounts of missing information. The last column and row
 #' contain row and column counts, respectively.
-#' @author Gerko Vink, 2018, based on an earlier version of the same function by
-#' Stef van Buuren, Karin Groothuis-Oudshoorn, 2000
+#' @author Mathis Brier, 2025, based on the original mice::md.pattern by Gerko Vink
+#' in 2018 and previous work of Stef van Buuren, Karin Groothuis-Oudshoorn, 2000
 #' @references Schafer, J.L. (1997), Analysis of multivariate incomplete data.
 #' London: Chapman&Hall.
 #'
@@ -37,14 +53,21 @@
 #' #   7   1   0   0   0  3
 #' #   0   8   9  10 27
 #' @export
-md.pattern <- function(x, plot = TRUE, rotate.names = FALSE) {
+md.pattern <- function(x, plot = TRUE, rotate.names = 0, pattern.ord = FALSE, min.ind = NULL, nb.pat = TRUE, nb.var = TRUE, nb.ind = TRUE, nb.tot = TRUE, names = colnames(x), colors = c("#006CC2B3", "#B61A51B3")) {
   if (!(is.matrix(x) || is.data.frame(x))) {
     stop("Data should be a matrix or dataframe")
   }
   if (ncol(x) < 2) {
     stop("Data should have at least two columns")
   }
+  if(length(names) != ncol(x)) { # Checking consistency of @param names
+    stop("Argument `names` should be the same size as the data")
+  }
+  if(!nb.var) { # to consistently remove the bottom numbers
+    nb.tot <- FALSE
+  }
   R <- is.na(x)
+  colnames(R) <- names # Assign names if provided
   nmis <- colSums(R)
   # sort columnwise
   R <- matrix(R[, order(nmis)], dim(x))
@@ -69,7 +92,13 @@ md.pattern <- function(x, plot = TRUE, rotate.names = FALSE) {
     }
     rownames(mpat) <- table(pat)
   }
+  if(!is.null(min.ind)) { # drop rows without enough individuals
+    mpat <- mpat[as.numeric(rownames(mpat)) > min.ind, ]
+  }
   r <- cbind(abs(mpat - 1), rowSums(mpat))
+  if(pattern.ord) { # order rows by number of missing variables
+    r <- r[order(r[, ncol(r)], decreasing = FALSE), ]
+  }  # binding then to be resilient if sum(nmis) would be smaller than max(rowSums(mpat))
   r <- rbind(r, c(nmis[order(nmis)], sum(nmis)))
   if (plot) {
     op <- par(mar = rep(0, 4))
@@ -82,37 +111,34 @@ md.pattern <- function(x, plot = TRUE, rotate.names = FALSE) {
         R <- t(as.matrix(R))
       }
       R <- r[1:nrow(r) - 1, 1:ncol(r) - 1]
-    }
-    if (rotate.names) {
-      adj <- c(0, 0.5)
-      srt <- 90
-      length_of_longest_colname <- max(nchar(colnames(r))) / 2.6
-      plot.window(
-        xlim = c(-1, ncol(R) + 1),
-        ylim = c(-1, nrow(R) + length_of_longest_colname),
-        asp = 1
-      )
-    } else {
-      adj <- c(0.5, 0)
-      srt <- 0
-      plot.window(
-        xlim = c(-1, ncol(R) + 1),
-        ylim = c(-1, nrow(R) + 1),
-        asp = 1
-      )
-    }
+    } # modified adj to allow various angles
+    adj <- c(0.5 - (rotate.names / 200 + 0.05), (rotate.names / 200 + 0.05))
+    srt <- rotate.names
+    length_of_longest_colname <- max(nchar(colnames(r)))
+    plot.window(
+      xlim = c(-1, ncol(R) + 1),
+      ylim = c(-1, nrow(R) + length_of_longest_colname),
+      asp = 1)
     M <- cbind(c(row(R)), c(col(R))) - 1
-    shade <- ifelse(R[nrow(R):1, ], mdc(1), mdc(2))
+    shade <- ifelse(R[nrow(R):1, ], colors[1], colors[2]) # changed to allow color variation
     rect(M[, 2], M[, 1], M[, 2] + 1, M[, 1] + 1, col = shade)
     for (i in 1:ncol(R)) {
-      text(i - .5, nrow(R) + .3, colnames(r)[i], adj = adj, srt = srt)
-      text(i - .5, -.3, nmis[order(nmis)][i])
+      text(i - 0.5, nrow(R) + .3, colnames(r)[i], adj = adj, srt = srt)
+      if(nb.var) { # changed to allow the deletion of individuals per variable - bottom numbers
+        text(i - .5, -.3, r[nrow(r), i])
+      }
     }
     for (i in 1:nrow(R)) {
-      text(ncol(R) + .3, i - .5, r[(nrow(r) - 1):1, ncol(r)][i], adj = 0)
-      text(-.3, i - .5, rownames(r)[(nrow(r) - 1):1][i], adj = 1)
+      if(nb.pat) { # changed to allow the deletion of number of variables per pattern - right numbers
+        text(ncol(R) + .3, i - .5, r[(nrow(r) - 1):1, ncol(r)][i], adj = 0)
+      }
+      if(nb.ind) { # changed to allow the deletion of individuals per pattern - left numbers
+        text(-.3, i - .5, rownames(r)[(nrow(r) - 1):1][i], adj = 1)
+      }
     }
-    text(ncol(R) + .3, -.3, r[nrow(r), ncol(r)])
+    if(nb.tot) { # change to allow deletion of total number of missing values - bottom-right number
+      text(ncol(R) + .3, -.3, r[nrow(r), ncol(r)])
+    }
     return(r)
   } else {
     return(r)
